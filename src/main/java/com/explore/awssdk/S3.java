60e10db61261f9781418ccc5468ceb5c51965c4a
Venu Krishnan <venutechie@gmail.com>
2022-01-04 22:27:21 -0500

    s3 upload file object

--- src/main/java/com/explore/awssdk/S3.java
+++ src/main/java/com/explore/awssdk/S3.java
@@ -1,8 +1,56 @@
 package com.explore.awssdk;
 
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.SdkClientException;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+
+import java.io.File;
+import java.nio.file.Files;
+
+//Source
+//https://docs.aws.amazon.com/code-samples/latest/catalog/java-s3-src-main-java-aws-example-s3-UploadObject.java.html
+
 public class S3 {
 
-    public static void main(String[] args) {
-        System.out.println("test");
+    public static void main(String[] args) throws Exception {
+        Regions clientRegion = Regions.US_EAST_1;
+        String bucketName = "testbucket";
+        String stringObjKeyName = "*** String object key name ***";
+        String fileObjKeyName = "git.pdf";
+        String fileName = "/Users/Downloads/git.pdf";
+
+        try {
+            //This code expects that you have AWS credentials set up per:
+            // https://docs.aws.amazon.com/sdk-for-java/v1/developer-guide/setup-credentials.html
+            AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
+                    .withRegion(clientRegion)
+                    .build();
+
+            // Upload a text string as a new object.
+            //s3Client.putObject(bucketName, stringObjKeyName, "Uploaded String Object");
+
+            // Upload a file as a new object with ContentType and title specified.
+            File file = new File(fileName);
+            PutObjectRequest request = new PutObjectRequest(bucketName, fileObjKeyName, file);
+            ObjectMetadata metadata = new ObjectMetadata();
+            String mimeType = Files.probeContentType(file.toPath());
+            metadata.setContentType(mimeType);
+            //metadata.addUserMetadata("title", "git.pdf");
+            request.setMetadata(metadata);
+            s3Client.putObject(request);
+        } catch (AmazonServiceException e) {
+            // The call was transmitted successfully, but Amazon S3 couldn't process
+            // it, so it returned an error response.
+            e.printStackTrace();
+        } catch (SdkClientException e) {
+            // Amazon S3 couldn't be contacted for a response, or the client
+            // couldn't parse the response from Amazon S3.
+            e.printStackTrace();
+        }
     }
+
 }
